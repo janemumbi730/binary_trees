0c0f70dcd2078db4ece29c4fa7c1533f4ac935d7
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-28 14:12:35 +0300

    betty error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,21 +9,21 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *n_node;
+	binary_tree_t *a;
 
 	if (parent == NULL)
 		return (NULL);
-	n_node = binary_tree_node(parent, value);
+	a = binary_tree_node(parent, value);
 
-	if (n_node == NULL)
+	if (a == NULL)
 	{
 		return (NULL);
 	}
 	if (parent->left != NULL)
 	{
-		n_node->left = parent->left;
-		n_node->left->parent = n_node;
+		a->left = parent->left;
+		a->left->parent = a;
 	}
-	parent->left = n_node;
-	return (n_node);
+	parent->left = a;
+	return (a);
 }
