14fefd7676ac05bf8925719badec633579ffbbae
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-29 17:34:20 +0300

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,20 +1,5 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - gets the balance factor of b tree
- * @tree: root node
- * Return: balance factor or 0 if tree is NULL
- */
-
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-	return (0);
-
-	return ((int)tranverse_rows
-	(tree->left) - (int)tranverse_rows(tree->right));
-}
-
-/**
  * tranverse_rows - gets rows of b tree
  * @tree: root node
  * Return: rows or 0 if tree is NULL
@@ -38,3 +23,19 @@ size_t tranverse_rows(const binary_tree_t *tree)
 
 	return ((a > b) ? (a) : (b));
 }
+
+/**
+ * binary_tree_balance - gets the balance factor of b tree
+ * @tree: root node
+ * Return: balance factor or 0 if tree is NULL
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	return (0);
+
+	return ((int)tranverse_rows
+	(tree->left) - (int)tranverse_rows(tree->right));
+}
+
