57a2b11bdad04bbcfe57bc2361cab9d9471929b0
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-30 12:02:03 +0300

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,26 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_sibling - gets sibling of node in b tree
+ * @node: sibling finder node
+ * Return: sibling node
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == NULL || node->parent->right == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+
+}
+
+/**
  * binary_tree_uncle - gets uncle of node in b tree
  * @node: uncle finder node
  * Return: uncle node
