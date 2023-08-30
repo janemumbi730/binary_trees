588f1d7d5aa9f91bbc7f12e70137ed7f005132bc
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-30 12:01:40 +0300

    error solved

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -19,24 +19,3 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (node->parent->left);
 
 }
-
-/**
- * binary_tree_sibling - gets sibling of node in b tree
- * @node: sibling finder node
- * Return: sibling node
- */
-
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
-{
-	if (node == NULL || node->parent == NULL)
-		return (NULL);
-
-	if (node->parent->left == NULL || node->parent->right == NULL)
-		return (NULL);
-
-	if (node->parent->left == node)
-		return (node->parent->right);
-	else
-		return (node->parent->left);
-
-}
