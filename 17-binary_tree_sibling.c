8298b2673aeecb7b6947a6dda272f3528e483dc3
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-30 11:47:01 +0300

    betty errror

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,14 +1,15 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_sibling - Finds the sibling of a node in a binary tree
- * @node: Pointer to the node to find the sibling
- * Return: Pointer to the sibling node
+ * binary_tree_sibling - gets sibling of node in b tree
+ * @node: sibling finder node
+ * Return: sibling node
  */
+
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
+
 	if (node->parent->left == NULL || node->parent->right == NULL)
 		return (NULL);
 
